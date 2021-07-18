0158259b3a6878f563f0cbbe1ec65f1c8d5c4902
Vanesa Gonzalez <vanesaglezrguez@gmail.com>
2021-07-18 15:40:29 +0200

    Arreglo pequeño error clase insecto

---
+++ ejerciciozoologico/EjercicioZoologico/Insecto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioZoologico
+{
+    class Insecto:Animal
+    {
+
+        protected bool vuela;
+
+        public Insecto(string especie, string nombreAnimal, double peso, int jaula, bool vuela)
+            : base(especie, nombreAnimal, peso, jaula)
+        {
+            this.vuela=vuela;
+        }
+
+        public override void queClaseDeAnimalEres()
+        {
+            if (vuela == true)
+            {
+                Console.WriteLine("Soy un insecto llamado: " + nombreAnimal + "\nde la especie: " + especie + "\nPeso en kg: "
+                + peso + "\nEstoy en la jaula: " + jaula + "\nY vuelo");
+            }
+            else
+            {
+                Console.WriteLine("Soy un insecto llamado: " + nombreAnimal + "\nde la especie: " + especie + "\nPeso en kg: "
+                + peso + "\nEstoy en la jaula: " + jaula + "\nY no vuelo");
+            }
+        }
+    }
+}
