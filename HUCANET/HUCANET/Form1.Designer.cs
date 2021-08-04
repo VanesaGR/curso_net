4c19d1d5741b2e7758ca8b6318aafcbc2dc07a3f
VANESA GONZALEZ <Tarde@ASATA406>
2021-08-04 17:11:04 +0200

    Funcionalidad botones

---
+++ HUCANET/HUCANET/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿
+namespace HUCANET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gridDatos = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtRegistro = new System.Windows.Forms.TextBox();
+            this.txtSS = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dpFecha = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.Sexo = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInscripcion = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDatos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(148, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(713, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HUCANET, PROYECTO DE GESTIÓN DE HOSPITALES";
+            // 
+            // gridDatos
+            // 
+            this.gridDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridDatos.Location = new System.Drawing.Point(544, 55);
+            this.gridDatos.Name = "gridDatos";
+            this.gridDatos.Size = new System.Drawing.Size(686, 623);
+            this.gridDatos.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnUpdate);
+            this.groupBox1.Controls.Add(this.btnRead);
+            this.groupBox1.Controls.Add(this.btnCreate);
+            this.groupBox1.Controls.Add(this.txtRegistro);
+            this.groupBox1.Controls.Add(this.txtSS);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtDireccion);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.dpFecha);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.rbFemale);
+            this.groupBox1.Controls.Add(this.rbMale);
+            this.groupBox1.Controls.Add(this.Sexo);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtInscripcion);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(3, 55);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(535, 623);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnExit.Location = new System.Drawing.Point(405, 563);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(103, 43);
+            this.btnExit.TabIndex = 18;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnDelete.Location = new System.Drawing.Point(305, 563);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(103, 43);
+            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnUpdate.Location = new System.Drawing.Point(207, 563);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(103, 43);
+            this.btnUpdate.TabIndex = 16;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnRead.Location = new System.Drawing.Point(107, 563);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(103, 43);
+            this.btnRead.TabIndex = 15;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCreate.Location = new System.Drawing.Point(9, 563);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(103, 43);
+            this.btnCreate.TabIndex = 14;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtRegistro
+            // 
+            this.txtRegistro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtRegistro.Location = new System.Drawing.Point(6, 503);
+            this.txtRegistro.Name = "txtRegistro";
+            this.txtRegistro.Size = new System.Drawing.Size(516, 23);
+            this.txtRegistro.TabIndex = 13;
+            // 
+            // txtSS
+            // 
+            this.txtSS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtSS.Location = new System.Drawing.Point(170, 401);
+            this.txtSS.Name = "txtSS";
+            this.txtSS.Size = new System.Drawing.Size(322, 23);
+            this.txtSS.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(16, 404);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Número de la SS";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtDireccion.Location = new System.Drawing.Point(151, 317);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(294, 23);
+            this.txtDireccion.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(26, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Dirección";
+            // 
+            // dpFecha
+            // 
+            this.dpFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dpFecha.Location = new System.Drawing.Point(136, 226);
+            this.dpFecha.Name = "dpFecha";
+            this.dpFecha.Size = new System.Drawing.Size(263, 23);
+            this.dpFecha.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(26, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Fecha";
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.rbFemale.Location = new System.Drawing.Point(229, 160);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(72, 21);
+            this.rbFemale.TabIndex = 6;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.rbMale.Location = new System.Drawing.Point(107, 160);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(56, 21);
+            this.rbMale.TabIndex = 5;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // Sexo
+            // 
+            this.Sexo.AutoSize = true;
+            this.Sexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Sexo.Location = new System.Drawing.Point(26, 164);
+            this.Sexo.Name = "Sexo";
+            this.Sexo.Size = new System.Drawing.Size(39, 17);
+            this.Sexo.TabIndex = 4;
+            this.Sexo.Text = "Sexo";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtNombre.Location = new System.Drawing.Point(174, 89);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(348, 23);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(3, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "NOMBRE Y APELLIDOS";
+            // 
+            // txtInscripcion
+            // 
+            this.txtInscripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtInscripcion.Location = new System.Drawing.Point(125, 20);
+            this.txtInscripcion.Name = "txtInscripcion";
+            this.txtInscripcion.Size = new System.Drawing.Size(329, 23);
+            this.txtInscripcion.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(10, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "INSCRIPCIÓN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1232, 679);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gridDatos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridDatos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView gridDatos;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtRegistro;
+        private System.Windows.Forms.TextBox txtSS;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dpFecha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.Label Sexo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInscripcion;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HUCANET/HUCANET/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HUCANET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataClasses1DataContext baseDatos = new DataClasses1DataContext();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargarGrid();
+        }
+
+        void cargarGrid()
+        {
+            var cargaGrid = from p in baseDatos.pacientes
+                            select p;
+            gridDatos.DataSource = cargaGrid;
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pacientes mypaciente = new pacientes();
+                mypaciente.inscripcion = txtInscripcion.Text;
+                mypaciente.nombre = txtNombre.Text;
+
+                if (rbMale.Checked == true)
+                {
+                    mypaciente.sexo = "H";
+                }
+                else
+                {
+                    mypaciente.sexo = "M";
+                }
+                mypaciente.fecha = dpFecha.Value;
+                mypaciente.direccion = txtDireccion.Text;
+                mypaciente.ss = txtSS.Text;
+                baseDatos.pacientes.InsertOnSubmit(mypaciente);
+                baseDatos.SubmitChanges();
+                
+                txtRegistro.Text = "Inserción de datos realizada";
+
+                var cargarGrid = from pacientes in baseDatos.pacientes select pacientes;
+                gridDatos.DataSource = cargarGrid;
+            }
+            catch
+            {
+                MessageBox.Show("Fallo en la inserción de datos");
+            }
+           
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtInscripcion.Text != "")
+                {
+                    pacientes mypaciente = baseDatos.pacientes.Single(p =>
+                     p.inscripcion == txtInscripcion.Text);
+
+                    baseDatos.pacientes.DeleteOnSubmit(mypaciente);
+                    baseDatos.SubmitChanges();
+
+                    txtRegistro.Text = "Eliminación de datos realizada";
+                    
+                    cargarGrid();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error en la eliminacion de los datos");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtInscripcion.Text != "")
+                {
+                    pacientes mypaciente = baseDatos.pacientes.Single(p =>
+                    p.inscripcion == txtInscripcion.Text);
+
+                    mypaciente.inscripcion = txtInscripcion.Text;
+                    mypaciente.nombre = txtNombre.Text;
+                    if (rbMale.Checked == true)
+                    {
+                        mypaciente.sexo = "H";
+                    }
+                    else
+                    {
+                        mypaciente.sexo = "M";
+                    }
+                    mypaciente.fecha = dpFecha.Value;
+                    mypaciente.direccion = txtDireccion.Text;
+                    mypaciente.ss = txtSS.Text;
+                    baseDatos.SubmitChanges();
+
+                    txtRegistro.Text = "Modificación de datos realizada";
+
+                    cargarGrid();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error en la modificación de los datos");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
