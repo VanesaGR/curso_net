ef1bf8af9fcc26459f1781b53dabcb79594c34eb
VANESA GONZALEZ <Tarde@ASATA406>
2021-08-05 17:19:38 +0200

    Creación de productos y pedidos (1 articulo)

---
+++ Productos/Productos/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            producto p1 = new producto(); //creacion del producto
+           
+            string opcion = ""; //opcion del menu
+            double cuenta=0, acumulador=0;
+            int pedido = 0;
+
+            do
+            {
+                Console.WriteLine("MENÚ:");
+                Console.WriteLine("1. Crear objeto");
+                Console.WriteLine("2. Hacer pedido");
+                Console.WriteLine("3. Mostrar el total + IVA");
+                Console.WriteLine("4. Salir");
+
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1": //para introducir los datos de los productos
+                        Console.WriteLine("Introduce el nombre");
+                        p1.Pnombre= Console.ReadLine();
+                        Console.WriteLine("Introduce el precio");
+                        p1.Pprecio= double.Parse(Console.ReadLine());
+                        Console.WriteLine("Introduce la categoria");
+                        p1.Pcategoria= Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case "2":
+                        Console.WriteLine("Cantidad a comprar");
+                        pedido = int.Parse(Console.ReadLine());
+                        Console.Clear();
+                        break;
+                    case "3": 
+                        cuenta = p1.Pprecio * pedido-((p1.Pprecio * pedido)*0.21);                        
+                        acumulador = acumulador + cuenta;                        
+                        p1.Mostrar();                   
+                        Console.WriteLine("El coste total del pedido es: " + acumulador);
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case "4":
+                        Console.WriteLine("Saliendo...");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Error en los datos");
+                        break;
+
+                }
+            } while (opcion != "4");
+        }
+    }
+}
---
+++ Productos/Productos/producto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productos
+{
+    class producto
+    {
+        private string nombre; //atributos
+        private double precio;
+        private string categoria;
+        
+        public string Pnombre //propiedades
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public double Pprecio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+
+        public string Pcategoria
+        {
+            get { return categoria; }
+            set { categoria = value; }
+        }
+
+        public producto() { } //constructor por defecto
+
+        public producto(string nombre, double precio, string categoria)//constructor con los tres parametros
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.categoria = categoria;
+        }
+
+        public producto(string nombre, double precio) //constructor con dos parametros
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+        }
+
+        public void Mostrar() //método
+        {
+            Console.WriteLine("Has comprado " +nombre+ " a "+precio+ " con categoria "+categoria);
+        }
+
+
+    }
+}
