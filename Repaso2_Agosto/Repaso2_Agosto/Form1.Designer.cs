ef6ccd1b174601f1f033136479d1a33b9f8c5b18
VANESA GONZALEZ <Tarde@ASATA406>
2021-08-19 19:49:33 +0200

    Terminados botones

---
+++ Repaso2_Agosto/Repaso2_Agosto/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿
+namespace Repaso2_Agosto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbEstadoCivil = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.gridDatos = new System.Windows.Forms.DataGridView();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(479, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CLIENTES";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(36, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DNI";
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtDNI.Location = new System.Drawing.Point(92, 46);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(148, 23);
+            this.txtDNI.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnBuscar.Location = new System.Drawing.Point(284, 36);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(89, 36);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(36, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "NOMBRE";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtNombre.Location = new System.Drawing.Point(126, 87);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(247, 23);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(36, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "APELLIDOS";
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtApellidos.Location = new System.Drawing.Point(126, 124);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(247, 23);
+            this.txtApellidos.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(36, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ESTADO CIVIL";
+            // 
+            // cbEstadoCivil
+            // 
+            this.cbEstadoCivil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.cbEstadoCivil.FormattingEnabled = true;
+            this.cbEstadoCivil.Items.AddRange(new object[] {
+            "SOLTERO/A",
+            "CASADO/A",
+            "SEPARADO/A",
+            "VIUDO/A"});
+            this.cbEstadoCivil.Location = new System.Drawing.Point(165, 159);
+            this.cbEstadoCivil.Name = "cbEstadoCivil";
+            this.cbEstadoCivil.Size = new System.Drawing.Size(206, 24);
+            this.cbEstadoCivil.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(37, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "TELÉFONO";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtTelefono.Location = new System.Drawing.Point(165, 202);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(206, 23);
+            this.txtTelefono.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label7.Location = new System.Drawing.Point(37, 242);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "E-MAIL";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtEmail.Location = new System.Drawing.Point(126, 242);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(245, 23);
+            this.txtEmail.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label8.Location = new System.Drawing.Point(39, 286);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(164, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "FECHA DE NACIMIENTO";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(209, 281);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(261, 23);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnListar.Location = new System.Drawing.Point(42, 332);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(93, 33);
+            this.btnListar.TabIndex = 16;
+            this.btnListar.Text = "LISTAR";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnRegistrar.Location = new System.Drawing.Point(141, 332);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(99, 33);
+            this.btnRegistrar.TabIndex = 17;
+            this.btnRegistrar.Text = "REGISTRAR";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnEliminar.Location = new System.Drawing.Point(246, 332);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(99, 33);
+            this.btnEliminar.TabIndex = 18;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnModificar.Location = new System.Drawing.Point(351, 332);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(106, 33);
+            this.btnModificar.TabIndex = 19;
+            this.btnModificar.Text = "MODIFICAR";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // gridDatos
+            // 
+            this.gridDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridDatos.Location = new System.Drawing.Point(42, 392);
+            this.gridDatos.Name = "gridDatos";
+            this.gridDatos.Size = new System.Drawing.Size(1102, 264);
+            this.gridDatos.TabIndex = 20;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn1.Location = new System.Drawing.Point(634, 269);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 34);
+            this.btn1.TabIndex = 21;
+            this.btn1.Text = "|<";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn2.Location = new System.Drawing.Point(724, 270);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 34);
+            this.btn2.TabIndex = 22;
+            this.btn2.Text = "<";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn3.Location = new System.Drawing.Point(815, 270);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 34);
+            this.btn3.TabIndex = 23;
+            this.btn3.Text = ">";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn4.Location = new System.Drawing.Point(907, 270);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 34);
+            this.btn4.TabIndex = 24;
+            this.btn4.Text = ">|";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1182, 668);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.gridDatos);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbEstadoCivil);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtDNI);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbEstadoCivil;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.DataGridView gridDatos;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+    }
+}
+
---
+++ Repaso2_Agosto/Repaso2_Agosto/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repaso2_Agosto
+{
+    public partial class Form1 : Form
+    {
+        //AÑADIMOS EL CONTEXTO DE ENITDADES DE CLIENTES
+        REPASO_AGOSTOEntities1 ClientesEntity = new REPASO_AGOSTOEntities1();
+        public Form1()
+        {
+            InitializeComponent();
+            //INVOCAMOS AL ENVENTO DE CARGAR GRID
+            Cargargrid();
+        }
+
+        //DEFINIMOS UN EVENTO PARA CARGAR EL GRID
+        public void Cargargrid()
+        {
+            gridDatos.DataSource = ClientesEntity.cliente.ToList();
+        }//cliente es la base de datos
+
+        private void btnRegistrar_Click(object sender, EventArgs e)//boton para agregar registro
+        {
+            try
+            {                
+                    cliente Misclientes = new cliente() //CREAMOS UN NUEVO CLIENTE CON LOS CONTENIDOS DE LOS TEXTBOX
+                    {
+                        DNI = txtDNI.Text,
+                        NOMBRES = txtNombre.Text,
+                        APELLIDOS = txtApellidos.Text,
+                        ESTADO_CIVIL = cbEstadoCivil.Text,
+                        TELEFONO = txtTelefono.Text,
+                        EMAIL = txtEmail.Text,
+                        FECHA_NACIMIENTO = dateTimePicker1.Value
+                    };
+
+                    //CONTEXTO DE ENTIDADES Y AÑADIMOS AL CLIENTE
+                    ClientesEntity.cliente.Add(Misclientes);
+                    ClientesEntity.SaveChanges(); //GUARDAR EL NUEVO REGISTRO EN LA BASE DE DATOS
+                    Cargargrid(); //REFRESCAMOS EL DATA GRID
+                
+            }
+            catch
+            {
+                if (txtDNI.Text == "" || txtNombre.Text != "" || txtApellidos.Text != "" || cbEstadoCivil.Text != "" ||
+                    txtTelefono.Text != "" || txtEmail.Text != "")
+                {
+                    MessageBox.Show("Error. Faltan datos");
+                }
+                else
+                {
+                    MessageBox.Show("Error, registro duplicado");
+                }
+            }
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string textoDNI = txtDNI.Text;
+            var BuscaCliente = ClientesEntity.cliente.Where(c => c.DNI == textoDNI).Single();
+
+            txtNombre.Text=BuscaCliente.NOMBRES;
+            txtApellidos.Text=BuscaCliente.APELLIDOS;
+            cbEstadoCivil.Text=BuscaCliente.ESTADO_CIVIL;
+            txtTelefono.Text=BuscaCliente.TELEFONO;
+            txtEmail.Text=BuscaCliente.EMAIL;
+            dateTimePicker1.Text=BuscaCliente.FECHA_NACIMIENTO.ToString();
+            
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string textoDNI = txtDNI.Text; //DEFINIMOS UNA VARIABLE QUE GUARDA EL DNI
+            //MÉTODO LAMBDA QUE CONSIGUE A UN CLIENTE POR SU DNI
+
+            //AQUEL DNI QUE COINCIDE CON TEXTO DNI
+            var deleteCliente = ClientesEntity.cliente.Where(c => c.DNI == textoDNI).Single();
+
+            ClientesEntity.cliente.Remove(deleteCliente); //BORRAMOS AL CLIENTE BUSCADO
+            ClientesEntity.SaveChanges();
+            Cargargrid();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {//DEBERIA ESTAR CONTROLADO
+         //DEFINE LA VARIABLE PARA EL DNI
+            //TODO: 
+            string textoDNI = txtDNI.Text;
+
+            //MEDIANTE LINQ OBTENEMOS EL CLIENTE POR EL DNI
+            cliente Misclientes = (from c in ClientesEntity.cliente where c.DNI == textoDNI select c).Single();
+            
+            //DEFINIMOS LOS ATRIBUTOS DEL OBJETO Misclientes   
+            //Misclientes.DNI = txtDNI.Text;
+            Misclientes.NOMBRES = txtNombre.Text;
+            Misclientes.APELLIDOS = txtApellidos.Text;
+            Misclientes.ESTADO_CIVIL = cbEstadoCivil.Text;
+            Misclientes.TELEFONO = txtTelefono.Text;
+            Misclientes.EMAIL = txtEmail.Text;
+            Misclientes.FECHA_NACIMIENTO = dateTimePicker1.Value;
+            
+            //HACEMOS SALVADO EN LA BASE DE DATOS
+            ClientesEntity.SaveChanges();
+            Cargargrid();
+        }
+
+        private void btn1_Click(object sender, EventArgs e) //PARA BUSCAR EL PRIMER REGISTRO DEL DATAGRID
+        {
+            string textoDNI = txtDNI.Text;
+            var BuscaCliente = ClientesEntity.cliente.First();
+
+            txtDNI.Text = BuscaCliente.DNI;
+            txtNombre.Text = BuscaCliente.NOMBRES;
+            txtApellidos.Text = BuscaCliente.APELLIDOS;
+            cbEstadoCivil.Text = BuscaCliente.ESTADO_CIVIL;
+            txtTelefono.Text = BuscaCliente.TELEFONO;
+            txtEmail.Text = BuscaCliente.EMAIL;
+            dateTimePicker1.Text = BuscaCliente.FECHA_NACIMIENTO.ToString();
+        }
+
+        private void btn4_Click(object sender, EventArgs e) //PARA BUSCAR EL ULTIMO REGISTRO DEL DATAGRID
+        {
+            string textoDNI = txtDNI.Text;
+            var BuscaCliente = ClientesEntity.cliente.ToList().Last();
+
+            txtDNI.Text = BuscaCliente.DNI;
+            txtNombre.Text = BuscaCliente.NOMBRES;
+            txtApellidos.Text = BuscaCliente.APELLIDOS;
+            cbEstadoCivil.Text = BuscaCliente.ESTADO_CIVIL;
+            txtTelefono.Text = BuscaCliente.TELEFONO;
+            txtEmail.Text = BuscaCliente.EMAIL;
+            dateTimePicker1.Text = BuscaCliente.FECHA_NACIMIENTO.ToString();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //DECLARAMOS UNA VARIABLE PARA INDICAR LA FILA ANTERIOR
+                int anterior = gridDatos.CurrentRow.Index - 1;
+
+                //NOS DESPLAZAMOS A LA FILA ANTERIOR
+                gridDatos.CurrentCell = gridDatos.Rows[anterior].Cells
+                    [gridDatos.CurrentCell.ColumnIndex];
+
+                //AQUI CARGAMOS EN EL TEXTBOX DEL DNI
+                txtDNI.Text = gridDatos.Rows[anterior].Cells[0].Value.ToString();
+                txtNombre.Text = gridDatos.Rows[anterior].Cells[1].Value.ToString();
+                txtApellidos.Text = gridDatos.Rows[anterior].Cells[2].Value.ToString();
+                cbEstadoCivil.Text = gridDatos.Rows[anterior].Cells[3].Value.ToString();
+                txtTelefono.Text = gridDatos.Rows[anterior].Cells[4].Value.ToString();
+                txtEmail.Text = gridDatos.Rows[anterior].Cells[5].Value.ToString();
+                dateTimePicker1.Text = gridDatos.Rows[anterior].Cells[6].Value.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Estás en la primera fila");
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //DECLARAMOS UNA VARIABLE PARA INDICAR LA FILA SIGUIENTE
+                int siguiente = gridDatos.CurrentRow.Index + 1;
+
+                //NOS DESPLAZAMOS A LA FILA SIGUIENTE
+                gridDatos.CurrentCell = gridDatos.Rows[siguiente].Cells[gridDatos.CurrentCell.ColumnIndex];
+
+                //AQUÍ CARGAMOS EN EL TEXTBOX DEL DNI
+                txtDNI.Text = gridDatos.Rows[siguiente].Cells[0].Value.ToString();
+                txtNombre.Text = gridDatos.Rows[siguiente].Cells[1].Value.ToString();
+                txtApellidos.Text = gridDatos.Rows[siguiente].Cells[2].Value.ToString();
+                cbEstadoCivil.Text = gridDatos.Rows[siguiente].Cells[3].Value.ToString();
+                txtTelefono.Text = gridDatos.Rows[siguiente].Cells[4].Value.ToString();
+                txtEmail.Text = gridDatos.Rows[siguiente].Cells[5].Value.ToString();
+                dateTimePicker1.Text = gridDatos.Rows[siguiente].Cells[6].Value.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Estás en la última fila");
+            }
+        }
+    }
+}
